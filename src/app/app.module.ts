b3038f485a09995e21e7330c36d09874c3b1c198
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-11-30 00:27:14 -0300

    Módulo de tarefas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { TarefasModule } from './tarefas';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, TarefasModule],
   providers: [],
   bootstrap: [AppComponent],
 })
