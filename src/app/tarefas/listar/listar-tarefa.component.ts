6bfe8a3d54d60e2de4700b3afcb3c2987aa832a0
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-12-05 00:36:30 -0300

    Implementada a listagem de tarefas

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Tarefa, TarefaService } from '../shared';
+
 @Component({
   selector: 'app-listar-tarefa',
   templateUrl: './listar-tarefa.component.html',
-  styleUrls: ['./listar-tarefa.component.css']
+  styleUrls: ['./listar-tarefa.component.css'],
 })
 export class ListarTarefaComponent implements OnInit {
+  tarefas: Tarefa[];
 
-  constructor() { }
+  constructor(private tarefaService: TarefaService) {}
 
   ngOnInit(): void {
+    this.tarefas = this.listarTodos();
   }
 
+  listarTodos(): Tarefa[] {
+    return this.tarefaService.listarTodos();
+  }
+
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+  }
 }
--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
 
 @NgModule({
+  imports: [CommonModule, RouterModule, FormsModule],
   declarations: [ListarTarefaComponent],
-  imports: [CommonModule],
   providers: [TarefaService],
 })
 export class TarefasModule {}
