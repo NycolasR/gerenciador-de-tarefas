afd96933cb2c5204c431de2b33e958f55dee05f9
Nycolas R. Alves <nycolasramon3@gmail.com>
2022-12-17 02:28:34 -0300

    Implementada edição de tarefas

--- src/app/tarefas/editar/editar-tarefa.component.ts
+++ src/app/tarefas/editar/editar-tarefa.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+
+import { Tarefa, TarefaService } from '../shared';
 
 @Component({
   selector: 'app-editar-tarefa',
   templateUrl: './editar-tarefa.component.html',
-  styleUrls: ['./editar-tarefa.component.css']
+  styleUrls: ['./editar-tarefa.component.css'],
 })
 export class EditarTarefaComponent implements OnInit {
+  @ViewChild('formTarefa', { static: true }) formTarefa: NgForm;
+  tarefa: Tarefa;
 
-  constructor() { }
+  constructor(
+    private tarefaService: TarefaService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    let id = +this.route.snapshot.params['id']; // o "+" converte o valor para numérico
+    this.tarefa = this.tarefaService.buscarPorId(id);
   }
 
+  atualizar(): void {
+    if (this.formTarefa.form.valid) {
+      this.tarefaService.atualizar(this.tarefa);
+      this.router.navigate(['/tarefas']);
+    }
+  }
 }
